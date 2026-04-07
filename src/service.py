550da882586524c0e92bbcbb984d0b19b167d506
elpequenosaltatapias <l.a.bartolome@gmail.com>
2026-04-07 18:11:27 +0200

    forzar fallo prueba unitaria

--- src/service.py
+++ src/service.py
@@ -1,20 +1,20 @@
-from src.storage import save_name
-
-def validate_name(name):
-    return isinstance(name, str) and len(name.strip()) >= 2
-
-def register_name(names, name):
-    if not validate_name(name):
-        raise ValueError("Nombre no válido")
-    return save_name(names, name)
-
-#Codigo para error de test unitario
 # from src.storage import save_name
 
 # def validate_name(name):
-#     return True
+#     return isinstance(name, str) and len(name.strip()) >= 2
 
 # def register_name(names, name):
 #     if not validate_name(name):
 #         raise ValueError("Nombre no válido")
 #     return save_name(names, name)
+
+# Codigo para error de test unitario
+from src.storage import save_name
+
+def validate_name(name):
+    return True
+
+def register_name(names, name):
+    if not validate_name(name):
+        raise ValueError("Nombre no válido")
+    return save_name(names, name)
