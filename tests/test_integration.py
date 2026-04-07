121b9acb2d19fa8969af9e03a3c59597f6717176
elpequenosaltatapias <l.a.bartolome@gmail.com>
2026-04-07 17:14:29 +0200

    Quitar cobertura, dejar funcionando

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,7 @@
-# from src.service import register_name
+from src.service import register_name
 
-# def test_register_name():
-#     data = []
-#     result = register_name(data, "Luis")
+def test_register_name():
+    data = []
+    result = register_name(data, "Luis")
 
-#     assert result == ["Luis"]
+    assert result == ["Luis"]
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,7 +1,7 @@
-# from src.service import validate_name
+from src.service import validate_name
 
-# def test_valid_name():
-#     assert validate_name("Ana") is True
+def test_valid_name():
+    assert validate_name("Ana") is True
 
-# def test_invalid_name():
-#     assert validate_name("A") is False
+def test_invalid_name():
+    assert validate_name("A") is False
